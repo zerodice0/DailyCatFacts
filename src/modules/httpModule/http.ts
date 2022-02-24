2f4ba884d6cf60bbc5a7010e9747c8c5097d2cdc
zerodice0 <zerodice0@gmail.com>
2022-02-24 23:50:35 +0900

    - throw error on http module.

--- src/modules/httpModule/http.ts
+++ src/modules/httpModule/http.ts
@@ -1,13 +1,14 @@
 export default class Http {
-  public async get<T>(url: string): Promise<T|null> {
-    let result: T|null = null;
+  public async get<T>(url: string): Promise<T | null> {
+    let result: T | null = null;
+
     try {
       const res = await fetch(url);
       if (res.ok) {
         result = await res.json();
       }
     } catch (e) {
-      console.error(e);
+      throw e;
     }
 
     return result;
